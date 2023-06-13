8b695f7802d4abd284fe56c6e593370a1af8d93f
Kd-20028 <keshawndavis472@gmail.com>
2023-06-13 12:28:18 -0500

    Fixed bug with decimal points

--- script.js
+++ script.js
@@ -1,155 +1,149 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const display = document.querySelector(".total"); 
-    const numbersBtn = document.querySelectorAll(".number"); 
-    const history = document.querySelector(".equation");
-    const addition = document.querySelector("#add"); 
-    const subtraction =  document.querySelector("#subtract"); 
-    const multiplication = document.querySelector("#multiply"); 
-    const division = document.querySelector("#divide"); 
-    const equals = document.querySelector("#equal"); 
-    const clear = document.querySelector(".clear"); 
-    const deleteBtn = document.querySelector(".delete"); 
-    const decimal = document.querySelector("#decimal"); 
+    const display = document.querySelector(".total"); // Reference to the display element
+    const numbersBtn = document.querySelectorAll(".number"); // Reference to number buttons
+    const history = document.querySelector(".equation"); // Reference to the history element
+    const addition = document.querySelector("#add"); // Reference to the addition button
+    const subtraction = document.querySelector("#subtract"); // Reference to the subtraction button
+    const multiplication = document.querySelector("#multiply"); // Reference to the multiplication button
+    const division = document.querySelector("#divide"); // Reference to the division button
+    const equals = document.querySelector("#equal"); // Reference to the equals button
+    const clear = document.querySelector(".clear"); // Reference to the clear button
+    const deleteBtn = document.querySelector(".delete"); // Reference to the delete button
+    const decimal = document.querySelector("#decimal"); // Reference to the decimal button
    
+    let displayValue = " "; // Current value displayed on the calculator
+    let historyValue = " "; // Current history value displayed on the calculator
 
-    let displayValue = " "; 
-    let historyValue = " "; 
-
-
-    function add(a, b){
+    // Arithmetic functions
+    function add(a, b) {
         return a + b; 
     }
 
-    function subtract(a, b){
+    function subtract(a, b) {
         return a - b; 
     }
 
-    function multiply(a, b){
+    function multiply(a, b) {
         return a * b;
     }
 
-    function divide(a, b){
-        if (b == 0){
-            return "ERROR"
+    function divide(a, b) {
+        if (b == 0) {
+            return "ERROR";
         }
         return a / b; 
     }
 
-
-    function operate(num1, operator, num2){
+    // Perform arithmetic operation based on operator and numbers
+    function operate(num1, operator, num2) {
         switch (operator) {
             case "+":
-            return add(+num1, +num2);
+                return add(+num1, +num2);
             case "-":
-            return subtract(num1, num2);
+                return subtract(num1, num2);
             case "*":
-            return multiply(num1, num2);
+                return multiply(num1, num2);
             case "/":
-            return divide(num1, num2);
+                return divide(num1, num2);
             default:
-            return "Error";
+                return "Error";
         }
     }
 
+    // Event handler for number buttons
     function handleNumberClick(event) {
         const clickedNumber = event.target.textContent;
         displayValue += clickedNumber;
-        console.log(displayValue);
         display.textContent = displayValue;
         historyValue += clickedNumber; 
         history.textContent = historyValue; 
     }
     
-
-   function setDisplay(input){
+    // Update the display with a new value
+    function setDisplay(input) {
         history.textContent = input; 
         historyValue = input; 
         display.textContent = input; 
         displayValue = input; 
-   }
+    }
     
-    
-    let equationString = "";  
+    let equationString = "";  // Stores the equation as a string
 
-    numbersBtn.forEach(function(number){
+    // Add event listeners to number buttons
+    numbersBtn.forEach(function(number) {
         number.addEventListener("click", handleNumberClick); 
         number.addEventListener("click", () => {
-             equationString += number.textContent; 
-
+            equationString += number.textContent; 
         }); 
     });
 
-
-    
+    // Add event listeners to arithmetic operator buttons
     addition.addEventListener("click", handleNumberClick); 
     addition.addEventListener("click", () => {
-          equationString += (" " + addition.textContent + " ") ; 
+        equationString += (" " + addition.textContent + " ") ; 
     }); 
 
     subtraction.addEventListener("click", handleNumberClick); 
     subtraction.addEventListener("click", () => {
-          equationString += (" " + subtraction.textContent + " ") ; 
+        equationString += (" " + subtraction.textContent + " ") ; 
     }); 
 
     multiplication.addEventListener("click", handleNumberClick); 
     multiplication.addEventListener("click", () => {
-          equationString += (" " + multiplication.textContent + " ") ; 
+        equationString += (" " + multiplication.textContent + " ") ; 
     }); 
 
     division.addEventListener("click", handleNumberClick); 
     division.addEventListener("click", () => {
-          equationString += (" " + division.textContent + " ") ; 
+        equationString += (" " + division.textContent + " ") ; 
     }); 
 
+    // Event listener for the equals button
     equals.addEventListener("click", handleNumberClick); 
     equals.addEventListener("click", () => {
-        equationString = equationString.replace(/\s+/g, '');
-        equationString = equationString.replaceAll(/(\d+(?:\.\d+)?)(?!\.)|(?<!\.)\b(\D+)\b(?!$)/g, "$1$2 ");
-        let equationList = equationString.split(" "); 
-        equationList = equationList.filter((str) => str !== '');
-        let answer = operate(+equationList[0], equationList[1], +equationList[2]);
-    
+        equationString = equationString.replace(/\s+/g, ''); // Remove extra spaces
+        equationString = equationString.replaceAll(/(\d+(?:\.\d+)?)(?!\.)|(?<!\.)\b(\D+)\b(?!$)/g, "$1$2 "); // Add spaces between numbers and non-numbers
+        let equationList = equationString.split(" "); // Split equation string into an array
+        equationList = equationList.filter((str) => str !== ''); // Remove empty strings from the array
+        let answer = operate(+equationList[0], equationList[1], +equationList[2]); // Perform the calculation
         
-        setDisplay(answer); 
+        setDisplay(answer); // Update the display with the answer
 
-        equationString = answer; 
-       
+        equationString = answer; // Store the answer as the new equation string
     }); 
 
+    // Event listener for the clear button
     clear.addEventListener("click", () => {
-        setDisplay(""); 
-        equationString = "";
-        
-    })
+        setDisplay(""); // Clear the display
+        equationString = ""; // Clear the equation string
+    });
 
+    // Event listener for the delete button
     deleteBtn.addEventListener("click", () => {
-        equationString = equationString.replace(/\s+/g, '');
-        if (equationString.length === 1){
+        equationString = equationString.replace(/\s+/g, ''); // Remove extra spaces
+        if (equationString.length === 1) {
             equationString = "";
-        }
-        else{
-            equationString = equationString.substring(0,equationString.length-1);
+        } else {
+            equationString = equationString.substring(0, equationString.length - 1);
         }
        
-        setDisplay(equationString); 
+        setDisplay(equationString); // Update the display with the modified equation string
 
-        equationString = equationString.replaceAll(/(\d+(?:\.\d+)?)(?!\.)|(?<!\.)\b(\D+)\b(?!$)/g, "$1$2 ");
+        equationString = equationString.replaceAll(/(\d+(?:\.\d+)?)(?!\.)|(?<!\.)\b(\D+)\b(?!$)/g, "$1$2 "); // Add spaces between numbers and non-numbers
+    });
 
-    })
-
-
+    // Event listener for the decimal button
     decimal.addEventListener("click", () => {
-        equationString = equationString.replace(/\s+/g, '');
-        if (equationString.includes(".")){
-            equationString = equationString; 
-        }
-        else{
-            equationString = equationString + ".";
-        }
-        setDisplay(equationString); 
-    })             
+        equationString = equationString.replace(/\s+/g, ''); // Remove extra spaces
 
-    
+        const lastOperand = equationString.split(/[\+\-\*\/]/).pop(); // Get the last operand
+      
+        if (!lastOperand.includes(".")) { // Check if the last operand already has a decimal point
+          equationString = equationString + ".";
+        }
+      
+        setDisplay(equationString); // Update the display with the modified equation string
+    });
 
     console.log(equationString); 
 });
